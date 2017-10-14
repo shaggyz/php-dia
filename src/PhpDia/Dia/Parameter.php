d97d39f5451307355e9496075a76bdcfded96011
Nicolas Daniel Palumbo <n@xinax.net>
2017-10-15 01:59:16 +0200

    Added parameter element

--- src/PhpDia/Dia/Parameter.php
+++ src/PhpDia/Dia/Parameter.php
@@ -4,8 +4,153 @@ namespace PhpDia\Dia;
 
 class Parameter implements RenderItem
 {
+    const TEMPLATE = 'parameter';
+    const UNKNOWN_KIND = 0;
+
+    /** @var string */
+    protected $name;
+
+    /** @var string */
+    protected $type;
+
+    /** @var string */
+    protected $value;
+
+    /** @var string */
+    protected $comment;
+
+    /** @var int */
+    protected $kind = self::UNKNOWN_KIND;
+
+    /**
+     * @param string $name
+     * @param string $type
+     */
+    private function __construct(string $name, string $type)
+    {
+        $this->name = $name;
+        $this->type = $type;
+    }
+
+    /**
+     * @param string $name
+     * @param string $type
+     * @return Parameter
+     */
+    public static function create(string $name, string $type) : Parameter
+    {
+        return new static($name, $type);
+    }
+
+    /**
+     * @return string
+     */
     public function render(): string
     {
+        return TemplateManager::create()->render(static::TEMPLATE, $this->getValues());
+    }
 
+    /**
+     * @return array
+     */
+    protected function getValues() : array
+    {
+        return [
+            'name' => $this->getName(),
+            'type' => $this->getType(),
+            'value' => $this->getValue(),
+            'comment' => $this->getComment(),
+            'kind' => $this->getKind()
+        ];
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     * @return Parameter
+     */
+    public function setName(string $name): Parameter
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param string $type
+     * @return Parameter
+     */
+    public function setType(string $type): Parameter
+    {
+        $this->type = $type;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getValue(): string
+    {
+        return $this->value;
+    }
+
+    /**
+     * @param string $value
+     * @return Parameter
+     */
+    public function setValue(string $value): Parameter
+    {
+        $this->value = $value;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getComment(): string
+    {
+        return $this->comment;
+    }
+
+    /**
+     * @param string $comment
+     * @return Parameter
+     */
+    public function setComment(string $comment): Parameter
+    {
+        $this->comment = $comment;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getKind(): int
+    {
+        return $this->kind;
+    }
+
+    /**
+     * @param int $kind
+     * @return Parameter
+     */
+    public function setKind(int $kind): Parameter
+    {
+        $this->kind = $kind;
+        return $this;
     }
 }
---
+++ src/PhpDia/Tests/Dia/ParameterTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Dia;
+
+use PhpDia\Dia\Parameter;
+use PHPUnit\Framework\TestCase;
+
+class ParameterTest extends TestCase
+{
+    public function testRenderParameter()
+    {
+        $parameter = Parameter::create('name', 'string')
+            ->setValue('nico')
+            ->setComment('Comment');
+
+        $this->assertEquals(static::$expectedParameter, $parameter->render());
+    }
+
+    static $expectedParameter = <<<EOL
+<dia:composite type="umlparameter">
+    <dia:attribute name="name">
+        <dia:string>#name#</dia:string>
+    </dia:attribute>
+    <dia:attribute name="type">
+        <dia:string>#string#</dia:string>
+    </dia:attribute>
+    <dia:attribute name="value">
+        <dia:string>#nico#</dia:string>
+    </dia:attribute>
+    <dia:attribute name="comment">
+        <dia:string>#Comment#</dia:string>
+    </dia:attribute>
+    <dia:attribute name="kind">
+        <dia:enum val="0"/>
+    </dia:attribute>
+</dia:composite>
+EOL;
+
+}
