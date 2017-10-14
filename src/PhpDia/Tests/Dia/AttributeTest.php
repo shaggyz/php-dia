5f8360550b88c4ecbd782a0cdb7b371b4f66f9f0
Nicolas Daniel Palumbo <n@xinax.net>
2017-10-15 00:20:12 +0200

    Added attribute test

---
+++ src/PhpDia/Tests/Dia/AttributeTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace PhpDia\Tests\Dia;
+
+use PHPUnit\Framework\TestCase;
+
+class AttributeTest extends TestCase
+{
+    public function testRenderPublicAttribute()
+    {
+        $this->assertTrue(false);
+    }
+
+    public function testRenderProtectedAttribute()
+    {
+        $this->assertTrue(false);
+    }
+
+    public function testRenderPrivateAttribute()
+    {
+        $this->assertTrue(false);
+    }
+}
